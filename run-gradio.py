e2c484bad964f0d99400494cfcefefb1a481a9f5
Abubakar Abid <a12d@stanford.edu>
2020-07-20 11:14:23 -0500

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -6,13 +6,14 @@ c = CamApp()
 inp = gr.inputs.Image(label="Input Image", shape=(512,512))
 out = gr.outputs.Image(label="Pose")
 title="Pose Estimation"
-description="A Tensorflow implementation of the Openpose algorithm. Handles " \
+description="Upload a picture of yourself or another person, and the Openpose algorithm will identify . Handles " \
             "multiple people."
 examples = [
     ["obama.jpg"],
     ["uswnt.jpg"]
 ]
-gr.Interface(c.process_frame, inp, out, title=title,
+thumbnail = "https://github.com/gradio-app/hub-openpose/blob/master/screenshot.png?raw=true"
+gr.Interface(c.process_frame, inp, out, title=title, thumbnail=thumbnail,
              description=description,
              examples=examples).launch()
 
